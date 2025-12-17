7e0d244a993070a60f37b309e43be3f011b3f5fe
agananya03 <agananya54126@gmail.com>
2025-12-17 19:02:09 +0530

    Prompting

---
+++ hello_world.js
@@ -0,0 +1,28 @@
+import Groq from "groq-sdk";
+import 'dotenv/config'
+const client = new Groq({
+  apiKey: process.env.GROQ_API_KEY,
+});
+
+async function runChat() {
+  const chatCompletion = await client.chat.completions.create({
+    messages: [
+        {role: 'system', content: `Your name is fastes AI`},
+      {
+        role: "user",
+        content: "Hey, How are you ? What is your name?",
+      },
+    ],
+    model: "llama-3.3-70b-versatile",
+  });
+
+  console.log(chatCompletion.choices[0].message.content);
+}
+
+runChat();
+
+
+
+//Few shot prompting: You give examples to LLM
+//Zero shot prompting: Direct question or tasks are given without prior examples.
+//Chain of thought: Model is encouraged to break down reasoning step by step before arriving at an answer.
